5c5cd788dfd653407b3d3a49d7cd5687083d1605
Adam Kennedy <adamk@cpan.org>
2005-04-20 04:57:25 +0000

    Import of ADAMK/Perl-MinimumVersion-0.01 from CPAN.
    gitpan-cpan-distribution: Perl-MinimumVersion
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ADAMK/Perl-MinimumVersion-0.01.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

---
+++ lib/Perl/MinimumVersion.pm
@@ -0,0 +1,217 @@
+package Perl::MinimumVersion;
+
+=pod
+
+=head1 NAME
+
+Perl::MinimumVersion - Find the minimum required Perl version for any code
+
+=head1 SYNOPSIS
+
+  # Create the checker object
+  $object = Perl::MinimumVersion->new( $filename );
+  $object = Perl::MinimumVersion->new( \$source );
+  $object = Perl::MinimumVersion->new( $Document );
+  
+  # Find the minimum version
+  $version = $object->minimum_version;
+
+=head1 DESCRIPTION
+
+C<Perl::MinimumVersion> takes Perl source code and calculates the minimum
+version of perl required to be able to run it. Because it is based on
+L<PPI>, it can do this without having to actually load the code.
+
+This first release only tests based on the syntax of your code.
+
+As this module develops, it will also be able to check explicitly
+specified versions via C<require 5.005;>, and trace module dependencies
+as needed.
+
+Amoungst other things, we hope to be able to build a test that is able to
+tell when the version needed based on the code syntax is higher than the
+version you explicitly specified (and thus, is a package bug).
+
+Using C<Perl::MinimumVersion> is dead simple, the synopsis pretty much
+covers it.
+
+=head1 METHODS
+
+=cut
+
+use strict;
+use UNIVERSAL 'isa';
+use version;
+use List::Util ();
+use PPI;
+use PPI::Util '_Document';
+
+use vars qw{$VERSION %CHECKS};
+BEGIN {
+	$VERSION = '0.01';
+
+	# Create the list of version checks
+	%CHECKS = (
+		any_our_variables => qv('5.006'),
+		);
+}
+
+
+
+
+
+#####################################################################
+# Constructor
+
+=pod
+
+=head2 new $filename | \$source | $PPI_Document
+
+The C<new> constructor creates a new version checking object for a
+L<PPI::Document>. You can also provide the document to be read as a
+file name, or as a C<SCALAR> reference containing the code.
+
+Returns a new C<Perl::MinimumVersion> object, or C<undef> on error.
+
+=cut
+
+sub new {
+	my $class    = ref $_[0] ? ref shift : shift;
+	my $Document = _Document(shift) or return undef;
+
+	# Create the object
+	my $self = bless {
+		Document => $Document,
+		}, $class;
+
+	$self;
+}
+
+=pod
+
+=head2 Document
+
+The C<Document> accessor can be used to get the L<PPI::Document> object
+back out of the version checker.
+
+=cut
+
+sub Document { $_[0]->{Document} }
+
+
+
+
+
+#####################################################################
+# Main Methods
+
+=pod
+
+=head2 minimum_version
+
+The C<minimum_version> method is the primary method for finding the
+minimum perl version required based on C<all> factors in the document.
+
+Future versions of this package are expected to also add the methods
+C<minimum_syntax_version>, C<minimum_explicit_version> and
+C<minimum_module_version> to handle the three ways in which the version
+dependency can be added.
+
+Returns a L<version> object, or C<undef> on error.
+
+=cut
+
+sub minimum_version {
+	my $self = _self(@_) or return undef;
+
+	# Always check in descending version order.
+	# By doing it this way, the version of the first check that matches
+	# is also the version of the document as a whole.
+	my $check = List::Util::first { $self->$_() }
+	            sort { $CHECKS{$b} <=> $CHECKS{$a} }
+	            keys %CHECKS;
+
+	# If nothing matches, we default to 5.004
+	$check and $CHECKS{$check} or qv("5.004");
+}
+
+
+
+
+
+#####################################################################
+# Version Check Methods
+
+sub any_our_variables {
+	shift->Document->find_any( sub {
+		$_[1]->isa('PPI::Statement::Variable')
+		and
+		$_[1]->type eq 'our'
+	} );
+}
+
+
+
+
+
+#####################################################################
+# Support Functions
+
+# Let sub be a function, object method, and static method
+sub _self {
+	isa(ref $_[0], __PACKAGE__) and return shift;
+	isa($_[0], __PACKAGE__)     and return shift->new(@_);
+	__PACKAGE__->new(@_);
+}
+
+1;
+
+=pod
+
+=head1 BUGS
+
+It's very early days, so this probably doesn't catch anywhere near enough
+syntax cases, and I personally don't know enough of them.
+
+B<However> it is esceedingly easy to add a new syntax check, so if you
+find something this is missing, copy and paste one of the existing
+5 line checking functions, modify it to find what you want, and report it
+to rt.cpan.org, along with the version needed.
+
+I don't even need an entire diff... just the function and version.
+
+=head1 TO DO
+
+- Write lots more version checkers
+
+- Write the explicit version checker
+
+- Write the recursive module descend stuff
+
+=head1 SUPPORT
+
+All bugs should be filed via the CPAN bug tracker at
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Perl-MinimumVersion>
+
+For other issues, or commercial enhancement or support, contact the author.
+
+=head1 AUTHORS
+
+Adam Kennedy (Maintainer), L<http://ali.as/>, cpan@ali.as
+
+=head1 SEE ALSO
+
+L<PPI>, L<version>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2005 Adam Kennedy. All rights reserved.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
---
+++ t/02_main.t
@@ -0,0 +1,72 @@
+#!/usr/bin/perl -w
+
+# Main testing for Perl::MinimumVersion
+
+use strict;
+use lib ();
+use UNIVERSAL 'isa';
+use File::Spec::Functions ':ALL';
+BEGIN {
+	$| = 1;
+	unless ( $ENV{HARNESS_ACTIVE} ) {
+		require FindBin;
+		$FindBin::Bin = $FindBin::Bin; # Avoid a warning
+		chdir catdir( $FindBin::Bin, updir() );
+		lib->import('blib', 'lib');
+	}
+}
+
+use Test::More tests => 14;
+use PPI;
+use Perl::MinimumVersion;
+
+sub version_is {
+	my $Document = PPI::Document->new( shift );
+	isa_ok( $Document, 'PPI::Document' );
+	my $Version = Perl::MinimumVersion->new( $Document );
+	isa_ok( $Version, 'Perl::MinimumVersion' );
+	is( $Version->minimum_version, shift, $_[0] || 'Version matches expected' );
+	$Version;
+}
+
+
+
+
+
+#####################################################################
+# Basic Testing
+
+# Constructor testing
+{
+	my $Version = Perl::MinimumVersion->new( \'print "Hello World!\n";' );
+	isa_ok( $Version, 'Perl::MinimumVersion' );
+	$Version = Perl::MinimumVersion->new( catfile( 't', '02_main.t' ) );
+	# version_is tests the final method
+
+	# Bad things
+	foreach ( [], {}, sub { 1 } ) { # Add undef as well after PPI 0.906
+		is( Perl::MinimumVersion->new( $_ ), undef, '->new(evil) returns undef' );
+	}
+}
+
+
+{
+my $Version = version_is( <<'END_PERL', '5.004', 'Hello World matches expected version' );
+print "Hello World!\n";
+END_PERL
+is( $Version->any_our_variables, '', '->any_our_variables returns false' );
+
+# This first time, lets double check some assumptions
+isa_ok( $Version->Document, 'PPI::Document' );
+isa_ok( $Version->minimum_version, 'version' );
+}
+
+# Try one with an 'our' in it
+{
+my $Version = version_is( <<'END_PERL', '5.006', '"our" matches expected version' );
+our $foo = 'bar';
+END_PERL
+is( $Version->any_our_variables, 1, '->any_our_variables returns true' );
+}
+
+1;
