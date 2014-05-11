3f2fe7633bb47c0d7c8f511b91017d7a2399cdf6
Neil Bowers <neil@bowers.com>
2014-05-11 20:55:52 +0000

    Import of NEILB/Perl-MinimumVersion-1.37 from CPAN.
    gitpan-cpan-distribution: Perl-MinimumVersion
    gitpan-cpan-version:      1.37
    gitpan-cpan-path:         NEILB/Perl-MinimumVersion-1.37.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

--- lib/Perl/MinimumVersion.pm
+++ lib/Perl/MinimumVersion.pm
@@ -1,5 +1,5 @@
 package Perl::MinimumVersion;
-$Perl::MinimumVersion::VERSION = '1.36';
+$Perl::MinimumVersion::VERSION = '1.37';
 =pod
 
 =head1 NAME
@@ -59,7 +59,7 @@ BEGIN {
 
 	# The primary list of version checks
 	%CHECKS = (
-        _stacked_labels         => version->new('5.014'),
+        # _stacked_labels         => version->new('5.014'),
 
 		_yada_yada_yada         => version->new('5.012'),
 		_pkg_name_version       => version->new('5.012'),
